209b42731315e7c6392b9e082f7ea12484782aec
osnaldy <osnaldy@hotmail.com>
2017-10-25 18:48:08 -0400

    Created a function to return the minimum number of coins

--- MinimumCoinSystem.java
+++ MinimumCoinSystem.java
@@ -1,13 +1,20 @@
 public class MinimumCoinSystem {
 
-    public int test(int value, int[] coins){
-        int min = 1;
-        for(int coin: coins){
-            if(value >= coin) {
-                return min + test(value-coin,coins);
+    public int test (int x, int[] coins) {
+
+        int min = x;
+
+        for (int coin: coins) {
+
+            if (x - coin >= 0) {
+
+                int c = test(x - coin, coins);
+                if (min > c + 1) {
+                    min = c + 1;
+                }
             }
         }
-        return min-1;
+        return min;
     }
 
     public static void main(String[] args) {
@@ -16,6 +23,6 @@ public class MinimumCoinSystem {
 
         MinimumCoinSystem minimumCoinSystem = new MinimumCoinSystem();
 
-        System.out.println("The minimum number of coins is: " + minimumCoinSystem.test(50, coins));
+        System.out.println("The minimum number of coins is: " + minimumCoinSystem.test(32, coins));
     }
 }
