c9d178e504cfc2e5ef25614abb2ba7fb01e5311b
osnaldy <osnaldy@hotmail.com>
2017-10-30 19:29:29 -0400

    Create a function that convert a given number to its string value

---
+++ GivenNumberReturnValueInWords.java
@@ -0,0 +1,68 @@
+public class GivenNumberReturnValueInWords {
+
+    private String [] singleDigit = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen", "fourteen", "fifteen","sixteen","seventeen","eighteen","nineteen"};
+    private String [] doubleDigit = {"twenty", "thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+    private String word = "";
+
+    public String convertNumberToWord(int n) {
+
+        if (n == 0) {
+            word = "zero";
+        }
+
+        else if (n <= 19) {
+            word = singleDigit[n-1];
+        }
+
+        else if (n <= 99) {
+
+            word += doubleDigit[n / 10 - 2];
+            n = n % 10;
+
+            if (n != 0) {
+                word += " " + singleDigit[n%10 -1];
+            }
+            else {
+
+                word += "";
+            }
+        }
+
+        else if (n < 1000) {
+
+            word += singleDigit[n / 100 - 1] + " hundred ";
+            n = n % 100;
+
+            if (n == 0) {
+
+                word += "";
+            }
+            else if (n <= 19) {
+
+                word += "and " + singleDigit[n % 100 - 1];
+            }
+
+            else if (n <= 99) {
+
+                word += "and " + doubleDigit[n / 10 - 2];
+                n = n % 10;
+
+                if (n != 0) {
+                    word += " " + singleDigit[n%10 -1];
+                }
+                else {
+
+                    word += "";
+                }
+            }
+        }
+        
+        return word;
+    }
+
+    public static void main(String[] args) {
+
+        GivenNumberReturnValueInWords givenNumberReturnValueInWords = new GivenNumberReturnValueInWords();
+        System.out.println(givenNumberReturnValueInWords.convertNumberToWord(898));
+    }
+}
